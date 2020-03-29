cb6e123b0e6b1d3f864088d90709c6b83a41c4de
Vladislav Sakharov <brodron@gmail.com>
2020-03-29 23:01:53 +0300

    beta rush commit

---
+++ src/script.js
@@ -0,0 +1,10 @@
+import './pages/index.css';
+import './images/image-03.png';
+import './images/Facebook.png';
+import './images/Git.png';
+import './images/logout.png';
+import './images/Rectangle 8.png';
+import './images/Rectangle_active.png';
+import './images/logout_black.png';
+import './images/trash.png';
+import './images/trash_inactive.png';
